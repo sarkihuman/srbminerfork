a7ff2a69073d6eded629c449fba0ed3ba3209560
sarkihuman <87797861+sarkihuman@users.noreply.github.com>
2021-07-31 22:50:25 -0400

    Update UPX_POCOupx-mix_pool.bat

--- UPX_POCOupx-mix_pool.bat
+++ UPX_POCOupx-mix_pool.bat
@@ -2,5 +2,5 @@
 cd %~dp0	
 	
 cls	
-SRBMiner-MULTI.exe --algorithm cn-extremelite/upx2 --pool mine.uplexapool.com:1111!us.uplexa.herominers.com:10470!uplexa.miner.rocks:30022 --wallet UPX1WuTKHTNDwBE8b6ULRcRoAwBQodGf4YEjp3x7BRRdc9Z4UVP5tHhHvMDHMkpfz3dPp6Jh6tk92bqe8k7QT1dq9D39sY1hce --password POCOUPX --cpu-threads 6 --disable-gpu --keep-alive true
+SRBMiner-MULTI.exe --algorithm cn-extremelite/upx2 --pool mine.uplexapool.com:1111!us.uplexa.herominers.com:10470!uplexa.miner.rocks:30022 --wallet UPX1WuTKHTNDwBE8b6ULRcRoAwBQodGf4YEjp3x7BRRdc9Z4UVP5tHhHvMDHMkpfz3dPp6Jh6tk92bqe8k7QT1dq9D39sY1hce --password POCO-DIS --keep-alive true
 pause 
