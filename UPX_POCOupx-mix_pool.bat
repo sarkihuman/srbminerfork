616684c6ffd7d2249f4742ffd4dd13aef1536099
sarkihuman <87797861+sarkihuman@users.noreply.github.com>
2021-07-31 22:49:15 -0400

    Add files via upload

---
+++ UPX_POCOupx-mix_pool.bat
@@ -0,0 +1,6 @@
+@echo off	
+cd %~dp0	
+	
+cls	
+SRBMiner-MULTI.exe --algorithm cn-extremelite/upx2 --pool mine.uplexapool.com:1111!us.uplexa.herominers.com:10470!uplexa.miner.rocks:30022 --wallet UPX1WuTKHTNDwBE8b6ULRcRoAwBQodGf4YEjp3x7BRRdc9Z4UVP5tHhHvMDHMkpfz3dPp6Jh6tk92bqe8k7QT1dq9D39sY1hce --password POCOUPX --cpu-threads 6 --disable-gpu --keep-alive true
+pause 
